d0a06b8908c60b9bd145fb5510ed8417d32bec2b
Vitoria F <vitoria.s.franca@gmail.com>
2024-02-08 15:50:04 -0300

    Primeira aula

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import logo from './assets/Logo NLW.svg'
+import NewNoteCard from './components/NewNoteCard'
+import NoteCard from './components/NoteCard'
+
+export default function App() {
+  return (
+    <div className='mx-auto max-w-6xl  my-12 space-y-6 '>
+      <img src={logo} alt='NLW Expert' />
+
+      <form className='w-full'>
+        <input
+          type="text"
+          placeholder='Busque por suas notas'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500' />
+      </form>
+
+      <div className='h-px bg-slate-700' />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard/>
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+
+      </div>
+    </div>
+  )
+}
